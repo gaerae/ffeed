08f2e13bad42ae05eadbcebe2c2de140486a13c2
gaerae <gaerae@gmail.com>
2018-01-31 20:38:09 +0900

    - Update to the latest version of VueJS v2.5.13 - Update to the latest version of Facebook SDK v2.12 - add goo.gl analytics link

--- assets/app.js
+++ assets/app.js
@@ -18,44 +18,44 @@ new Vue({
   },
 
   created: function () {
-    if (facebookAppId == '{app-id}') {
-      this.mainMessage = 'You need to create a Facebook App ID.'
-      console.error('assets/app.js > line 2: facebookAppId')
+    if (facebookAppId === '{app-id}') {
+      this.mainMessage = 'You need to create a Facebook App ID.';
+      console.error('assets/app.js > line 2: facebookAppId');
       return false
     }
-    FB.init({appId: facebookAppId, version: 'v2.8', status: true, cookie: true, xfbml: true})
-    this.parseURL(window.location.search)
-    this.apiCall('apiFeedInit')
-    this.onInfiniteList()
+    FB.init({appId: facebookAppId, version: 'v2.12', status: true, cookie: true, xfbml: true});
+    this.parseURL(window.location.search);
+    this.apiCall('apiFeedInit');
+    this.onInfiniteList();
   },
 
   watch: {
     searchQuery: function() {
-      this.searchResult = null
-      this.feedInfo = null
-      this.feedList = null
-      self.mainMessage = ''
-      this.apiCall('apiSearch')
+      this.searchResult = null;
+      this.feedInfo = null;
+      this.feedList = null;
+      self.mainMessage = '';
+      this.apiCall('apiSearch');
     }
   },
 
   methods: {
     // parse a url
     parseURL: function(url) {
-      var tempParsing = [], tempSplit, tempUrls = url.slice(url.indexOf('?') + 1).split('&');
+      let tempParsing = [], tempSplit, tempUrls = url.slice(url.indexOf('?') + 1).split('&');
       for (var i = 0; i < tempUrls.length; i++) {
           tempSplit = tempUrls[i].split('=');
           tempParsing[tempSplit[0]] = tempSplit[1];
       }
-      this.feedId = (tempParsing['id']) ? tempParsing['id'] : null
-      this.feedLimit = (tempParsing['limit']) ? tempParsing['limit'] : this.feedLimit
+      this.feedId = (tempParsing['id']) ? tempParsing['id'] : null;
+      this.feedLimit = (tempParsing['limit']) ? tempParsing['limit'] : this.feedLimit;
     },
 
     // parse a message string
     parseMessage: function(message) {
-      message = message || ''
-      var urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i
-      var hashPattern = /(#[^\s]+)/g
+      message = message || '';
+      const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+      const hashPattern = /(#[^\s]+)/g;
       return message.replace(urlPattern, function(url){
         return '<a href="'+url+'" target="_blank">'+url+'</a>'
       }).replace(hashPattern, function(hashtag){
@@ -63,42 +63,50 @@ new Vue({
       })
     },
 
+    // parse a message string
+    parseClickAnalytics: function(url) {
+      const urlCheck = /(https:\/\/goo\.gl\/)/i;
+      if (urlCheck.test(url)) {
+        return '<a href="https://goo.gl/#analytics/'+encodeURI(url.replace('https://', ''))+'/all_time" target="_blank">Click Analytics</a> / ';
+      }
+    },
+
     // infinite list
     onInfiniteList: function(){
-      var self = this
+      const self = this;
       window.addEventListener('scroll', function(e) {
         if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
           if (self.ticking === false) {
-            self.apiCall('apiInfiniteList')
-            self.ticking = true
+            self.apiCall('apiInfiniteList');
+            self.ticking = true;
           }
         } else {
-          self.ticking = false
+          self.ticking = false;
         }
       })
     },
 
     // api call
     apiCall: function(callBackFunction) {
-      var self = this
-      self.loadingMessage = true
+      const self = this;
+      self.loadingMessage = true;
       FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
           self[callBackFunction]()
         } else {
-          self.mainMessage = 'A simple page that returns the feed of a specific facebook page and group.<br/>You need to log in facebook account.'
+          self.mainMessage = 'A simple page that returns the feed of a specific facebook page and group.<br/>You need to log in facebook account.';
         }
-        self.loadingMessage = false
+        self.loadingMessage = false;
       })
 
     },
 
     // search
     apiSearch: function() {
-      var self = this
-      if (self.searchQuery === null || self.searchQuery == '') {
-        self.searchResult = null
-        self.mainMessage = 'Please enter search values to perform search.'
+      const self = this;
+      if (self.searchQuery === null || self.searchQuery === '') {
+        self.searchResult = null;
+        self.mainMessage = 'Please enter search values to perform search.';
         return false
       }
       FB.api('/', 'post',
@@ -107,28 +115,28 @@ new Vue({
         ]},
         function (response) {
           if (response && !response.error) {
-            const tempPage = JSON.parse(response[0].body)
-            const tempGroup = JSON.parse(response[1].body)
-            const tempResult = []
-            let pageResultCount = tempPage.data.length + 1
+            const tempPage = JSON.parse(response[0].body);
+            const tempGroup = JSON.parse(response[1].body);
+            const tempResult = [];
+            let pageResultCount = tempPage.data.length + 1;
             tempGroup.data.forEach(function(data){
-              tempResult[pageResultCount] = data
-              pageResultCount++
-            })
-            self.searchResult = Object.assign({}, tempPage.data, tempResult)
+              tempResult[pageResultCount] = data;
+              pageResultCount++;
+            });
+            self.searchResult = Object.assign({}, tempPage.data, tempResult);
           } else {
-            self.searchResult = null
-            self.mainMessage = 'No data.<br/>Error message: '+response.error.message
+            self.searchResult = null;
+            self.mainMessage = 'No data.<br/>Error message: '+response.error.message;
           }
       })
     },
 
     // feed init
     apiFeedInit: function() {
-      var self = this
-      if (this.feedId === null || self.searchQuery == '') {
-        self.mainMessage = 'Please enter search values to perform search.'
-        return false
+      const self = this;
+      if (this.feedId === null || self.searchQuery === '') {
+        self.mainMessage = 'Please enter search values to perform search.';
+        return false;
       }
       FB.api(
         '/'+self.feedId,
@@ -140,47 +148,47 @@ new Vue({
             name: response.name,
             link: '//www.facebook.com/'+response.id,
             picture: response.picture.data.url
-          }
+          };
           if (response && !response.error) {
             if ((typeof response.posts === "object") && (response.posts !== null)) {
               // page
-              self.feedList = response.posts
+              self.feedList = response.posts;
             } else if ((typeof response.feed === "object") && (response.feed !== null)) {
               // group
-              self.feedList = response.feed
+              self.feedList = response.feed;
             } else {
-              return false
+              return false;
             }
             // total feed count
             self.feedTotalCount = self.feedList.data.length + 1
           } else {
-            self.feedList = null
-            self.mainMessage = 'No data.<br/>Error message: '+response.error.message
+            self.feedList = null;
+            self.mainMessage = 'No data.<br/>Error message: '+response.error.message;
           }
       })
     },
 
     // infinite list
     apiInfiniteList: function() {
-      var self = this
+      const self = this;
       if (self.feedList !== null) {
         FB.api(
           self.feedList.paging.next,
           function (response) {
             if (response && !response.error) {
-              const tempData = []
+              const tempData = [];
               response.data.forEach(function(data){
-                tempData[self.feedTotalCount] = data
-                self.feedTotalCount++
-              })
-              self.feedList.data = Object.assign({}, self.feedList.data, tempData)
-              self.feedList.paging = Object.assign({}, self.feedList.paging, response.paging)
-              self.ticking = false
+                tempData[self.feedTotalCount] = data;
+                self.feedTotalCount++;
+              });
+              self.feedList.data = Object.assign({}, self.feedList.data, tempData);
+              self.feedList.paging = Object.assign({}, self.feedList.paging, response.paging);
+              self.ticking = false;
             } else {
-              self.mainMessage = 'No data.<br/>Error message: '+response.error.message
+              self.mainMessage = 'No data.<br/>Error message: '+response.error.message;
             }
         })
       }
     }
   }
-})
+});
